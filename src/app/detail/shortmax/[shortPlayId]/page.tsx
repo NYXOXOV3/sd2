8b02fa8a0cf71682e450b76d10f6c582aca48f9e
NYXOXOV3 <fortressjr25@gmail.com>
2026-03-12 12:49:32 +0700

    Import useVideoInteractions hook in ShortMaxDetailPage

--- src/app/detail/shortmax/[shortPlayId]/page.tsx
+++ src/app/detail/shortmax/[shortPlayId]/page.tsx
@@ -6,6 +6,7 @@ import { Play, ChevronLeft } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
 import { useRouter, useParams } from "next/navigation";
+import { useVideoInteractions } from "@/hooks/useVideoInteractions";
 
 export default function ShortMaxDetailPage() {
   const params = useParams<{ shortPlayId: string }>();
